ef9e753497ba9a6908854f3ea4f36a14cc46fbc0
MaddeySh <maddeysharma96@gmail.com>
2021-04-23 16:52:56 +0530

    Sidebar work

---
+++ lesson-5/visa/src/components/Sidebar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { ListGroup } from 'react-bootstrap'
+
+export const Sidebar = () => {
+    return (
+        <ListGroupoup>
+            <ListGroup.Item>Profile</ListGroup.Item>
+            <ListGroup.Item>Home</ListGroup.Item>
+            <ListGroup.Item>About us</ListGroup.Item>
+            <ListGroup.Item>Gallery</ListGroup.Item>
+            <ListGroup.Item>Academics</ListGroup.Item>
+            <ListGroup.Item>Achievements</ListGroup.Item>
+            <ListGroup.Item>Books</ListGroup.Item>
+            <ListGroup.Item>Contact</ListGroup.Item>
+            <ListGroup.Item>Our Social sites</ListGroup.Item>
+            <ListGroup.Item>Log Out</ListGroup.Item>
+        </ListGroup>
+    )
+}
--- lesson-5/visa/src/components/index.js
+++ lesson-5/visa/src/components/index.js
@@ -2,5 +2,6 @@ import { Header } from "./Header";
 import { Main } from "./Main";
 import { Footer } from "./Footer";
 import { PrivateLayout, PublicLayout } from "./Layout";
+import { Sidebar } from "./Sidebar";
 
-export { Header, Main, Footer, PrivateLayout, PublicLayout };
+export { Header, Main, Footer, PrivateLayout, PublicLayout , Sidebar };
