acb115daf8419121b4ac7d8b48ad95b49206141c
SonyDaman <sonydaman@gmail.com>
2021-04-23 20:10:51 +0530

    update public layout

--- lesson-5/visa/src/components/Layout/PublicLayout.jsx
+++ lesson-5/visa/src/components/Layout/PublicLayout.jsx
@@ -7,8 +7,13 @@ export const PublicLayout = (props) => {
   return (
     <div className="public-layout">
       <Header />
-        {props.children}
-      <Sidebar />
+      <div className="container">
+            <div className="row">
+                <div className="col-4"><Sidebar /></div>
+                <div className="col-8">{props.children}</div>
+            </div>
+        </div>
+        
       <Footer />
     </div>
   );
--- lesson-5/visa/src/components/Sidebar.jsx
+++ lesson-5/visa/src/components/Sidebar.jsx
@@ -4,23 +4,18 @@ import { ListGroup } from 'react-bootstrap'
 
 export const Sidebar = () => {
     return (
-        <div className="container">
-            <div className="row">
-                <div className="col-4">
-                    <ListGroup>
-            <ListGroup.Item>Profile</ListGroup.Item>
-            <ListGroup.Item>Home</ListGroup.Item>
-            <ListGroup.Item>About us</ListGroup.Item>
-            <ListGroup.Item>Gallery</ListGroup.Item>
-            <ListGroup.Item>Academics</ListGroup.Item>
-            <ListGroup.Item>Achievements</ListGroup.Item>
-            <ListGroup.Item>Books</ListGroup.Item>
-            <ListGroup.Item>Contact</ListGroup.Item>
-            <ListGroup.Item>Our Social sites</ListGroup.Item>
-            <ListGroup.Item>Log Out</ListGroup.Item>
-        </ListGroup>
-                </div>
-            </div>
-        </div>
+        
+            <ListGroup>
+                <ListGroup.Item>Profile</ListGroup.Item>
+                <ListGroup.Item>Home</ListGroup.Item>
+                <ListGroup.Item>About us</ListGroup.Item>
+                <ListGroup.Item>Gallery</ListGroup.Item>
+                <ListGroup.Item>Academics</ListGroup.Item>
+                <ListGroup.Item>Achievements</ListGroup.Item>
+                <ListGroup.Item>Books</ListGroup.Item>
+                <ListGroup.Item>Contact</ListGroup.Item>
+                <ListGroup.Item>Our Social sites</ListGroup.Item>
+                <ListGroup.Item>Log Out</ListGroup.Item>
+            </ListGroup>
     )
 }
