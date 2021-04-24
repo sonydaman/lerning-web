ea758ed57d08164687924b4ea4d2a57ebe7f83bc
SonyDaman <sonydaman@gmail.com>
2021-04-24 20:49:19 +0530

    Add font awesome

--- lesson-5/visa/src/components/Sidebar.jsx
+++ lesson-5/visa/src/components/Sidebar.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import { ListGroup } from 'react-bootstrap'
-
-
+import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAdjust } from '@fortawesome/free-solid-svg-icons'
+// https://fontawesome.com/icons?d=gallery&p=2&s=solid
 export const Sidebar = () => {
     return (
         
             <ListGroup>
                 <ListGroup.Item>Profile</ListGroup.Item>
                 <ListGroup.Item>Home</ListGroup.Item>
-                <ListGroup.Item>About us</ListGroup.Item>
+                <ListGroup.Item>
+                    <Link to="/about">
+                        <FontAwesomeIcon icon={faAdjust} />
+                        About us
+                    </Link>
+                </ListGroup.Item>
                 <ListGroup.Item>Gallery</ListGroup.Item>
                 <ListGroup.Item>Academics</ListGroup.Item>
                 <ListGroup.Item>Achievements</ListGroup.Item>
