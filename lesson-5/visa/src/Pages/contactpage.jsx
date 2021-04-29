f0ad9a361856e31282558b0b52ab8cbba73b1796
MaddeySh <maddeysharma96@gmail.com>
2021-04-29 19:31:48 +0530

    fix in academic page

--- lesson-5/visa/src/Pages/contactpage.jsx
+++ lesson-5/visa/src/Pages/contactpage.jsx
@@ -1,18 +1,9 @@
 import React from 'react'
-import { Col, Row } from 'reactbootstarp'
 export const ContactPage = () => {
     return (
         <div>
-            <Row>
-                {
-                    images.map((imagesId, index)
-
-
-                    )
-                }
-
-
-            </Row>
+          
+                Hy
         </div>
 
     )
