2317445c4c2631c2bddc07a2d8865ba2e3ea7ff6
suraj-kumar-sunny <suraj.kumarengg14321@gmail.com>
2021-05-02 13:04:58 +0530

    add story tab and browser link in main

--- lesson-5/visa/src/components/Main.jsx
+++ lesson-5/visa/src/components/Main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../style/main.css'
-import { Button, Form, FormControl, Spinner } from 'react-bootstrap';
+import { Button, Card, CardDeck, Form, FormControl } from 'react-bootstrap';
 import { BreadCrumb } from '.';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHeartbeat, faShareSquare } from '@fortawesome/free-solid-svg-icons'
@@ -9,20 +9,62 @@ import { faHeartbeat, faShareSquare } from '@fortawesome/free-solid-svg-icons'
 
 export const Main = () => {
   const UserFullName = ["Rohit Sharma","Krishan Kumar", "Manpreet Singh","Aditya Chouhan"]
-    return (   
-      <>
-           {
-               UserFullName.length === 0 && (
-                   <Spinner animation="border" role="status">
-                        <span className="sr-only">Loading...</span>
-                    </Spinner>
-               )
-           } 
+      return (   
       <div className='main-div'>
         
         <BreadCrumb pageName="Dashboard" />
         <h4>All Posts</h4>
         <div className="all-post">
+        <CardDeck>
+          <Card>
+            <Card.Img variant="top" src="https://unsplash.it/190/280?id=1" />
+            <Card.Body>
+              <Card.Title>Sonu Yadav</Card.Title>
+              <Card.Text>
+                This is my new status.
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Recently Updated</small>
+            </Card.Footer>
+          </Card>
+          <Card>
+            <Card.Img variant="top" src="https://unsplash.it/190/280?id=2" />
+            <Card.Body>
+              <Card.Title>Gurpreet Singh</Card.Title>
+              <Card.Text>
+                Hi Friends...
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+          </Card>
+          <Card>
+            <Card.Img variant="top" src="https://unsplash.it/190/280?id=3" />
+            <Card.Body>
+              <Card.Title>Shahrukh Khan</Card.Title>
+              <Card.Text>
+                Good Morning Fans..!
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Last updated 21 hours ago</small>
+            </Card.Footer>
+          </Card>
+          <Card>
+            <Card.Img variant="top" src="https://unsplash.it/190/280?id=4" />
+            <Card.Body>
+              <Card.Title>Rahul</Card.Title>
+              <Card.Text>
+                Feeling happy 
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Last updated yesterday</small>
+            </Card.Footer>
+          </Card>
+        </CardDeck>
           <div className="post-new">
           <div className="mb-3">
             <Form.File id="formcheck-api-custom" custom>
@@ -72,6 +114,5 @@ export const Main = () => {
   )}
   </div>
 </div>
-</>
 )
 }
