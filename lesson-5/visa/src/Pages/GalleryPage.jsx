529e92f08cc96aefe76e3a3883b118cca24e302f
manvinder <singhmanvinder852@gmail.com>
2021-05-03 20:49:46 +0530

    update gallery

--- lesson-5/visa/src/Pages/GalleryPage.jsx
+++ lesson-5/visa/src/Pages/GalleryPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Col, Row } from 'react-bootstrap'
+import { BreadCrumb } from '../Components/BreadCrumb'
 export const GalleryPage = () => {
     return (
         <div>
