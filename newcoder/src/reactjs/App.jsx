cadebf004fb7817812ff72493229c6bcc52bfdd1
parteek kumar <parteekarora007@gmail.com>
2021-05-03 14:13:00 +0530

    changes

---
+++ newcoder/src/reactjs/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+const App =() => {
+   
+
+    return(
+
+      <>
+            <h1>   welcome </h1>
+      </>
+
+
+
+    );
+
+};
+export default App;
---
+++ newcoder/src/reactjs/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import {BrowserRouter} from 'react-routor-dom';
+
+ReactDOM.render(
+    <>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </> ,   
+  document.getElementById('root')
+);
