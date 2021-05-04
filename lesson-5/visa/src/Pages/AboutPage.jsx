2221ed49f77bf19e956f07a3a801866154863586
MaddeySh <maddeysharma96@gmail.com>
2021-05-04 20:46:04 +0530

    About Page nd login page

--- lesson-5/visa/src/Pages/AboutPage.jsx
+++ lesson-5/visa/src/Pages/AboutPage.jsx
@@ -1,6 +1,8 @@
 
 import React from 'react'
 import { BreadCrumb } from '../Components/BreadCrumb';
+
+
 import "../style/AboutPage.css";
 export const AboutPage = () => {
     return (
---
+++ lesson-5/visa/src/Pages/LoginPage.jsx
@@ -0,0 +1,53 @@
+import { Button } from 'bootstrap'
+import React from 'react'
+import { Col, Form, FormLabel, Row } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+
+export const LoginPage = () => {
+    return(
+        
+        <div className="Login-Box">
+            <div className="Login-logo">
+                <Link to "#"></div>FUNBOOK</Link>
+            </div>
+            <div className="form-container">
+                <p>Sign in to start your session</p>
+                <Form>
+                    <Row>
+                        <div>
+                    <input type="email" className="input-field" placeholder="Email" name="email"/>
+                        </div>
+                        <div>
+                    <input type="password" className="input-field" placeholder="Password" name="Password"/>
+                        </div>
+                    </Row>
+                    <Row>
+                        <Col>
+                        <input type="checkbox" className="form-check-input" id="dropdownCheck"/>
+                        <FormLabel className="form-check-label" for="dropdownCheck">
+                            Remember me
+                        </FormLabel>
+                        </Col>
+                        <Col>
+                        <Button></Button>
+                        
+                        <button type="submit">Sign in</button>
+                        </Col>
+                    </Row>
+                </Form>
+            </div>
+        </div>
+
+    )
+}
+                 //   </form>
+                  //  <div class="Social-authority-link">
+                  //      <p class="or">-OR-</p>
+                  //      <a  class="btn btn-block btn-social btn-facebook btn-flat col-12" href="#">
+                  //          <i class="fa fa-facebook col-2"></i>Sign in using facebook</a>
+                  //       <a class="btn btn-block btn-social btn-Google btn-flat col-12" href="#">
+                  //           <i class="fa fa-google-plus col-2"></i>Sign in using Google+</a>
+                  //      </div>
+                  //      <a class="page" href="#">I forget my Password</a></br>
+                  //      <a class="page" href="#">Register a new Membership</a>
+                  //  </div>
