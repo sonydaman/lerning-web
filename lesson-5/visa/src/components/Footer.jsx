dba5a16943a8acb7195715d4f8053eff66623da9
MaddeySh <maddeysharma96@gmail.com>
2021-05-04 20:09:28 +0530

    footer

--- lesson-5/visa/src/components/Footer.jsx
+++ lesson-5/visa/src/components/Footer.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Col, Row } from 'react-bootstrap'
+import "../style/footer.css"
 
 export const Footer = () => {
     return (
-        <div className="footer">
+        <div className="footer container-fluid" >
                 <Row>
                     <Col lg={6} md={6} sm={6}>
                         <div className="Copy-right-text">
