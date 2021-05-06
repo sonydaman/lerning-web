3e49aa8e26451fd6d00df6bf3acfa80c3297ab51
parteek kumar <parteekarora007@gmail.com>
2021-05-06 19:04:11 +0530

    new components

---
+++ lesson-5/visa/src/components/Alerts.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Alerts = () => {
+    return (
+        <div>
+            <div className="alert alert-primary" role="alert">
+             A simple primary alert—check it out!
+            </div>
+
+        </div>
+    )
+}
+
+export default Alerts
