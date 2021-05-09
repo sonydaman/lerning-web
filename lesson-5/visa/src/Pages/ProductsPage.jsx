8d0bd5793ef68e4c7b5b4b6f4291a41c169a8fcb
sonydaman <sonydaman@gmail.com>
2021-05-10 00:16:52 +0530

    first stage

---
+++ lesson-5/visa/src/Pages/ProductsPage.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import propTypes from 'prop-types'
+import Achievement from '../Components/Achievement'
+import Achievements from '../Components/Achievement'
+
+const ProductsPage = props => {
+    return (
+        <div>
+            <h1 className="update">update</h1>
+            <div className="key-img">
+                <img src="https://unsplash.it/850/200?id=1" alt=""></img>
+            </div>
+        </div>
+
+
+
+
+
+    )
+}
+
+ProductsPage.propTypes = {
+
+}
+export default ProductsPage
