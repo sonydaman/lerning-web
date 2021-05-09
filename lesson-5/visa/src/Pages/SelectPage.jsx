5de9f0d6ebffd297e6342730e188910f1d8620f1
sonydaman <sonydaman@gmail.com>
2021-05-10 00:42:34 +0530

    select test

---
+++ lesson-5/visa/src/Pages/SelectPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BreadCrumb } from '../Components'
+// import { Link } from 'react-router-dom'
+
+export const SelectPage = () => {
+    return (
+
+        <div className="academic-page">
+            <BreadCrumb />
+            <h2>Academic</h2>
+            this is a test
+        </div>
+
+    )
+}
+
