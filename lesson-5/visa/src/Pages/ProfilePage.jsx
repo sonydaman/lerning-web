62d7eb5cb4cbb5fe5b3e102e791853c117c6d21a
sonydaman <sonydaman@gmail.com>
2021-05-10 21:37:02 +0530

    profile set

---
+++ lesson-5/visa/src/Pages/ProfilePage.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import propTypes from 'prop-types'
+import Achievement from '../Components/Achievement'
+import Achievements from '../Components/Achievements'
+
+const ProfilePage = props => {
+    return (
+        <div>
+            <h1 className="profile">profile</h1>
+            <div className="img">
+                <img src="https:unsplash.it/200/200"></img>
+
+            </div>
+
+        </div>
+
+    )
+}
+
+ProfilePage.propTypes = {
+
+}
+export default ProfilePage
