4d2a630f0cbccd5a48f38346c8f91e80e65610f3
sonydaman <sonydaman@gmail.com>
2021-05-11 01:57:55 +0530

    login test

---
+++ lesson-5/visa/src/Pages/LoginPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import LoginComponent from '../Components/LoginComponent'
+const LoginPage = props => {
+    return (
+        <div className="container">
+            uiiiuui
+        </div>
+    )
+}
+
+LoginPage.propTypes = {
+
+}
+
+export default LoginPage
