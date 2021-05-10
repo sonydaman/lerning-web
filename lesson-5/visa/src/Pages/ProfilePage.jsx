ea82da101c179a04d8b5136b00ba69cc7f8b64b7
sonydaman <sonydaman@gmail.com>
2021-05-11 00:30:31 +0530

    profile set

--- lesson-5/visa/src/Pages/ProfilePage.jsx
+++ lesson-5/visa/src/Pages/ProfilePage.jsx
@@ -9,9 +9,90 @@ const ProfilePage = props => {
             <h1 className="profile">profile</h1>
             <div className="img">
                 <img src="https:unsplash.it/200/200"></img>
-
+                <img src="https:unsplash.it/200/200"></img>
+                <img src="https:unsplash.it/200/200"></img>
+                 <img src="https:unsplash.it/200/200"></img>
+               
             </div>
+            <div className="desc">
+            An Instagram bio is the small area under your username where you can share some details about yourself or your brand. Your Instagram bio can include a brief self or brand description, contact information, emojis, hashtags and more. 
+            Here’s an example of one of the best bios on Instagram:
+            Marie Forleo has a strong Insta bio. The first line is used to describe who she is: ‘CEO. Writer. #MarieTV. Fancy Dancer.’
+            The second line of her cool bio is used for social proof: ‘Named by Oprah “Thought leader for next generation.”’ I don’t know about you, but to me, this says that she’s worth following. 
+            The third line of this short bio for Instagram describes what she does: ‘Learn to get *anything* you want.’ 
+            And the last line is her CTA, which links to a free content download. She also included two eBECOME A MEMBERInsgam Bio IdeasHOME/BLOG/
+   200+ INSTAGRAM BIO IDEAS YOU CAN COPY AND PASTE
+   ARTICLE by Nicole Martins Ferreira
+   21 Jul, 2020
+   Skip to article content
+   POST CONTENTS1
+   What is an Instagram Bio?2
+   Instagram Bio Template to Follow
+1. Include a Description of Yourself
+2. List Your Interests
+3. Share Your Contact Information
+4. Add a Call to Action3
+4 Instagram Bio Tricks to Try
+1. Instagram Bio Fonts
+2. Instagram Bio Symbols
+3. Instagram Bio Link
+4. Instagram Bio Space4
+   200+ Instagram Bio Ideas That’ll Up Your Instagram Game
+   Cool Instagram Bios
+   Best Instagram Bios
+   Creative Instagram Bios
+   Instagram Bio Ideas With Emoji
+   Instagram Bio Ideas for Girls
+   Instagram Bio Ideas for Boys
+   Best Instagram Bio to Get Followers
+   Great Instagram Bios
+   Instagram Bio Puns
+   Simple Bio for Insram
+   Funny Instagram Bios
+   Cute Instagram Bios
+   Perfect Bio for Instagram
+   Business Bio for Instagram
+   Bio Captions
+   Myself Quotes for Instagram Bio
+   Swag Bio for Instagra
+   Instagram Bio Quotes
+   Instagram Bio Quotes Short
+   Instagram Bio Text
+   Instagram Bio Inspiration
+   Instagram Bio for Fashion
+   Fitness Bio for Instagram
+   Instagram Bio for Models
+   Travel Bio for Instagram5
+   Instagram Bio Examples
+   6Get More Action with a Killer Instagram Bio7
+   Want to Learn More?
+   Just like pretty much everything else on the internet, your Instagram bio is all about making that impactful first impression. 
+   Most people only take a few seconds to scan bio and photos before deciding whether or not they should follow you. If your Instagram bio is enticing, they might decide to engage with your content or follow your account. If it isn’t, you might lose their interest… forever. 
+   Okay, well, maybe not forever. But at least until you make some changes. 
+   In this article, we’ll break down what a bio is, how to write one, and some cool Instagram bio tricks. But most importantly, we’ll give you some Instagram bio ideas that you can literally copy and paste onto your profile. 
+   (Don’t worry, we won’t tell anyone — it’ll be our little secret.)
+   Our Instagram course helps you build your Instagram account from scratch, give your audience the content they want to see, AND get sales.
+   BUILD A KILLER INSTAGRAM STRATEGY
+   What is an Instagram Bio?
+   An Instagram bio is the small area under your username where you can share some details about yourself or your brand. Your Instagram bio can include a brief self or brand description, contact information, emojis, hashtags and more. 
+   Here’s an example of one of the best bios on Instagram:
+   Marie Forleo has a strong Insta bio. The first line is used to describe who she is: ‘CEO. Writer. #MarieTV. Fancy Dancer.’
+   The second line of her cool bio is used for social proof: ‘Named by Oprah “Thought leader for next generation.”’ I don’t know about you, but to me, this says that she’s worth following. 
+   The third line of this short bio for Instagram describes what she does: ‘Learn to get *anything* you want.’ 
+   And the last line is her CTA, which links to a free content download. She also included two emojis to make the text easier to read and draw extra attention. 
+   If you’re looking for Instagram bio ideas for girls, Marie Forleo’s Instagram bio formula is worth copying.
+   marie forleo instagram bio
+   Instagram Bio Template to Follow
+   You can write your Instagram bio any way you want to. Here are a few Instagram bio tips that you might want to add based on popular trends. If you want to become an Instagram master, follow these tips and sign up for the Instagram course today.
+1. Include a Description of Yourself
+  The best Instagram bios tend to have a self-description. Things to consider adding: your job title(s), the company you own or work for, some claims to fame, things you love, and what makes you stand out.
+  Queen Latifah’s Instagram bio does a great job of describing her. In the first line of her bio she writes, ‘Actor/Rapper/Singer/Producer.’ When people visit her fanpage, they’ll learn that she’s more than just an actor and may check out her other gigs to learn more about her. 
+  Plus, that little blue tick shows up on her page, so people will immediately understand that she’s famous when visiting her Instagram account. One of the Instagram bio tips Queen Latifah teaches us is that you can learn a lot about a person even from a short bio.
 
+
+               
+            </div>
+            
         </div>
 
     )
