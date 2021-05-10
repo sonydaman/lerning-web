077c6204b7a61c51eb47939eb55b99a8a71c2455
sonydaman <sonydaman@gmail.com>
2021-05-11 02:02:32 +0530

    logintest

--- lesson-5/visa/src/Pages/LoginPage.jsx
+++ lesson-5/visa/src/Pages/LoginPage.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+//import PropTypes from 'prop-types'
 import LoginComponent from '../Components/LoginComponent'
 const LoginPage = props => {
     return (
         <div className="container">
-            uiiiuui
+
+            <LoginComponent title="log in start your work" btntext="signin" inputtext="email" inputText="password" isRemember />
         </div>
     )
 }
