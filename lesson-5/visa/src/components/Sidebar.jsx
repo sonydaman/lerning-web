bda49962ca0c78de37cc49bd7e13444a42e2070a
sonydaman <sonydaman@gmail.com>
2021-05-11 23:17:57 +0530

    sidebar set

--- lesson-5/visa/src/components/Sidebar.jsx
+++ lesson-5/visa/src/components/Sidebar.jsx
@@ -1,63 +1,28 @@
 import React from 'react'
 import { ListGroup } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import LinkComponent from './LinkComponent'
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // import { faAdjust } from '@fortawesome/free-solid-svg-icons'
 // https://fontawesome.com/icons?d=gallery&p=2&s=solid
 export const Sidebar = () => {
+    const items = [
+            {url:'/main',linkText:'Dashboard'},
+            {url:'/about',linkText:'About us'},
+            {url:'/profile',linkText:'Profile'},
+            {url:'/gallery',linkText:'Gallery'},
+            {url:'/academic',linkText:'Academic'},
+            {url:'/achievements',linkText:'Achievements'},
+            {url:'/books',linkText:'Books'},
+            {url:'/home',linkText:'HomePage'},
+            {url:'/contact',linkText:'ContactPage'},
+            {url:'/signin',linkText:'Sign In'},
+        ]
     return (
 
         <ListGroup>
-            <ListGroup.Item>
-                <Link to="/main">
-                    Dashboard
-                    </Link>
-            </ListGroup.Item>
-            <ListGroup.Item>Profile</ListGroup.Item>
-            <ListGroup.Item>
-                <Link to="/about">
-                    {/* <FontAwesomeIcon icon={faAdjust} /> */}
-                        About us
-                    </Link>
-            </ListGroup.Item>
-            <ListGroup.Item>
-                <Link to="/gallery">
-                    {/* <FontAwesomeIcon icon={faAdjust} /> */}
-                        Gallery
-                    </Link>
-            </ListGroup.Item>
-            <ListGroup.Item>
-                <Link to="/academic">
-                    {/* <FontAwesomeIcon icon={faAdjust} /> */}
-                        Academic
-                    </Link>
-
-            </ListGroup.Item>
-            <ListGroup.Item>
-                <Link to="/achievements">
-                    Achievements
-                        </Link>
-            </ListGroup.Item>
-            <ListGroup.Item>
-                <Link to="/books">
-                    Books
-                    </Link>
-            </ListGroup.Item>
-            <ListGroup.Item>
-                <Link to="/home">
-                    HomePage
-                    </Link>
-            </ListGroup.Item>
-            <ListGroup.Item>
-                <Link to="/contact">
-                    ContactPage
-                    </Link>
-            </ListGroup.Item>
-            <ListGroup.Item>
-                <Link to="/signin">
-                    Signin
-                    </Link>
-            </ListGroup.Item>
+            <LinkComponent items={items} />
+            {/*
             <ListGroup.Item>
                 <Link to="/contact">
                     Contact
@@ -147,7 +112,7 @@ export const Sidebar = () => {
             </ListGroup.Item>
 
             <ListGroup.Item></ListGroup.Item>
-            <ListGroup.Item>Log Out</ListGroup.Item>
+            <ListGroup.Item>Log Out</ListGroup.Item> */}
         </ListGroup>
     )
 }
