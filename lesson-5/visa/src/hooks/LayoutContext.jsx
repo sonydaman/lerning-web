9dc5960126b1df38b86f8b569fa26e484529acdc
sonydaman <sonydaman@gmail.com>
2021-05-12 01:26:49 +0530

    hook change

---
+++ lesson-5/visa/src/hooks/LayoutContext.jsx
@@ -0,0 +1,26 @@
+import { createContext, useContext } from 'react'
+const LayoutContext = createContext();
+const useLayout = useContext(LayoutContext);
+const initialState = { count: 0, userProfile: {}, isLoggedIn: false };
+
+function LayoutReducer(state, action) {
+    switch (action.type) {
+        case "login":
+            return { initialState, isLoggedIn: true };
+        case "increase":
+            return { count: state.count + 1 };
+        case "decrease":
+            return { count: state.count - 1 };
+        default:
+            throw new Error();
+    }
+}
+
+const LayoutProvider = LayoutContext.Provider;
+export {
+    LayoutContext,
+    LayoutProvider,
+    useLayout,
+    LayoutReducer,
+    initialState,
+};
