35f9fec559e750828ec77a67d2a52bfd5f68063b
sonydaman <sonydaman@gmail.com>
2021-05-11 12:53:34 +0530

    shift test

---
+++ lesson-5/visa/src/Pages/GayatriPage.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+const GayatriPage = props => {
+    return (
+        <>
+            <div className="img1">
+                <img src="https://unsplash.it/50/50"></img>
+                <h1>Gayatri Tutorial</h1>
+                <img src="https://unsplash.it/50/50"></img>
+                <h1>Gayatri Tutorial</h1>
+            </div>
+            <div className="img1">
+                <h4>introduction</h4>
+                aim<img src="https://unsplash.it/200/200"></img>
+            </div>
+        </>
+    )
+
+}
+GayatriPage.prototype = {
+
+}
+export default GayatriPage
