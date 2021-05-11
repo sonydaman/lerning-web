353eb21b4f355f064eddd88247c8ab14717c191b
sonydaman <sonydaman@gmail.com>
2021-05-11 23:17:09 +0530

    share set

--- lesson-5/visa/src/components/Layout/PublicLayout.jsx
+++ lesson-5/visa/src/components/Layout/PublicLayout.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Footer, Header } from "..";
 import { Sidebar } from "../Sidebar";
+
 export const PublicLayout = (props) => {
+
   return (
     <div className="public-layout">
       <Header />
