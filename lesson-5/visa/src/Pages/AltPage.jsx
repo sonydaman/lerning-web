a687dd13b982d8b7c0c63e0cc259e60716513897
sonydaman <sonydaman@gmail.com>
2021-05-11 13:16:57 +0530

    alt key

---
+++ lesson-5/visa/src/Pages/AltPage.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const AltPage = props => {
+    return (
+        <>
+            <div classname="test1">
+                <h1>AltKey</h1>
+                <h3>key1</h3>
+                <img src="https:unsplash.it/200/200"></img>
+
+            </div>
+            <div className="test2">
+                <h3>key2</h3>
+                <img src="https:unsplash.it/200/200"></img>
+
+            </div>
+            <div className="test3">
+                <h3>key3</h3>
+                <img src="https:unsplash.it/200/200"></img>
+
+            </div>
+
+            <div className="test4">
+                <h3>key4</h3>
+                <img src="https:unsplash.it/200/200"></img>
+
+            </div>
+
+        </>
+    )
+}
+
+AltPage.propTypes = {
+
+}
+
+export default AltPage
+
