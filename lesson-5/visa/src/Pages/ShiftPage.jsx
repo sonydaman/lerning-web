b07516a8682ecf65fef75f4f0a13b7a262aa5e3f
sonydaman <sonydaman@gmail.com>
2021-05-11 11:29:12 +0530

    shift set

---
+++ lesson-5/visa/src/Pages/ShiftPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const ShiftPage = props => {
+    return (
+        <div>
+            this is a page
+        </div>
+    )
+}
+
+
+.propTypes = {
+
+}
+
+export default
+
