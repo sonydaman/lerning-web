07716988cbace50788bd9774ee1a1d25eea6e831
sonydaman <sonydaman@gmail.com>
2021-05-11 19:27:14 +0530

    poduct set

--- lesson-5/visa/src/Pages/ProductPage.jsx
+++ lesson-5/visa/src/Pages/ProductPage.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { BreadCrumb, Card, Img } from '../Components'
-
 export const ProductPage = () => {
     return (
         <div>
@@ -10,7 +9,6 @@ export const ProductPage = () => {
             Magni, similique sint? Dignissimos, culpa tenetur! Doloribus similique, dolores, dignissimos dolor amet voluptatibus odio molestiae quod voluptas beatae enim velit sunt voluptate quo. Autem aspernatur illum, reiciendis hic corporis omnis.
             Harum explicabo modi maxime numquam quibusdam totam, animi excepturi quas ex omnis debitis vel, doloremque cumque quaerat dolorum qui maiores. Tenetur saepe voluptatum omnis eos ratione dolorum veritatis sed delectus.
             Id, quam, delectus quas nobis neque commodi dolore dicta repellat iste tempora quod nulla deserunt, quo fugit laudantium debitis. Voluptate magni impedit sapiente autem sunt nam dolores minus architecto voluptatum." />
-
                 <Card title="Product 2" image="https://unsplash.it/200/250?id=6" description="Ratione aliquam, quisquam autem id quaerat tempore! Est porro laborum natus ex quos. Quos dicta accusamus nam! Libero velit possimus, perferendis laboriosam ducimus debitis? Earum veniam dolor libero accusantium temporibus? Alias sunt officiis magni debitis ipsam recusandae qui quibusdam exercitationem pariatur iusto eius sint ea aperiam, dolorem voluptatibus itaque saepe autem modi illum reiciendis, iste dignissimos delectus nobis et. Mollitia. Eligendi vitae veniam sed pariatur quae cumque et inventore doloribus mollitia, illum dolor ipsum dolore cum nihil reiciendis fugiat, blanditiis impedit. Voluptatum esse cumque quam obcaecati doloribus totam, nihil" />
                 <Card title="Product 3" image="https://unsplash.it/200/250?id=8" description="Suscipit provident fugit alias quos aut impedit, eum doloremque porro repudiandae rem aliquid! Illo, dolor libero! Eum mollitia asperiores placeat, ullam iure impedit quibusdam assumenda earum at debitis iste fugit reprehenderit libero nisi dolores facilis corporis ipsam iusto magnam delectus unde quasi sapiente. Totam, inventore odit? Quia tenetur, mollitia ex deserunt porro amet delectus corrupti veritatis soluta minus, suscipit asperiores cupiditate explicabo sed maiores deleniti cum dolore nihil ut exercitationem! Maiores, eius minus! Vero, libero maiores? Voluptas provident ullam optio exercitationem! Provident nihil numquam iste tempora perferendis excepturi nesciunt, non veniam nobis nisi fugiat ex quos ducimus itaque ipsam deserunt impedit accusantium! Consequuntur. Et laboriosam dolore iusto, sequi" />
 
