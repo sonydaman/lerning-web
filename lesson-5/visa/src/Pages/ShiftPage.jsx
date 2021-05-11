ff621405af925447f4d94ff1c41b74829c498726
sonydaman <sonydaman@gmail.com>
2021-05-11 12:53:06 +0530

    shift test

--- lesson-5/visa/src/Pages/ShiftPage.jsx
+++ lesson-5/visa/src/Pages/ShiftPage.jsx
@@ -3,16 +3,31 @@ import PropTypes from 'prop-types'
 
 const ShiftPage = props => {
     return (
-        <div>
-            this is a page
-        </div>
+        <>
+            <div className="head">
+                <h1>test1</h1>
+                <img src="https:unsplash.it/200/200"></img>
+            </div>
+            <div className="test">
+                <h1>test2</h1>
+                <img src="https:unsplash.it/200/200"></img>
+            </div>
+            <div className="test">
+                <h1>test3</h1>
+                <img src="https:unsplash.it/200/200"></img>
+            </div>
+            <div className="test">
+                <h1>test4</h1>
+                <img src="https:unsplash.it/200/200"></img>
+            </div>
+        </>
     )
 }
 
 
-.propTypes = {
+ShiftPage.propTypes = {
 
 }
 
-export default
+export default ShiftPage
 
