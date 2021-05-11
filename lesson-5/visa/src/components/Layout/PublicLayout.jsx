0e552950c766cc4722ec87f34a5ee5e44ace0bbd
sonydaman <sonydaman@gmail.com>
2021-05-11 23:06:51 +0530

    layout set

--- lesson-5/visa/src/components/Layout/PublicLayout.jsx
+++ lesson-5/visa/src/components/Layout/PublicLayout.jsx
@@ -1,19 +1,17 @@
 import React from "react";
 import { Footer, Header } from "..";
 import { Sidebar } from "../Sidebar";
-
 export const PublicLayout = (props) => {
-
   return (
     <div className="public-layout">
       <Header />
       <div className="container-fluid">
-            <div className="row">
-                <div className="col-2"><Sidebar /></div>
-                <div className="col-8">{props.children}</div>
-            </div>
+        <div className="row">
+          <div className="col-2"><Sidebar /></div>
+          <div className="col-8">{props.children}</div>
         </div>
-        
+      </div>
+
       <Footer />
     </div>
   );
