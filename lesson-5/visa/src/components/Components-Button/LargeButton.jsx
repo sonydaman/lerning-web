bce2fe51bff0305cfb9273b6c7c471fa5580ed4f
sonydaman <sonydaman@gmail.com>
2021-05-12 18:44:46 +0530

    large

---
+++ lesson-5/visa/src/components/Components-Button/LargeButton.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const LargeButton = props => {
+    return (
+        <div>
+            <button className="button">
+                largebutton
+            </button>
+        </div>
+    )
+}
+
+LargeButton.propTypes = {
+
+}
+
+export default LargeButton
