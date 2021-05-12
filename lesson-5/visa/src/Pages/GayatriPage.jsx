d2e8bd78833cf4d93272041bb9601a86dc4e0111
sonydaman <sonydaman@gmail.com>
2021-05-12 19:24:19 +0530

    gyatri

--- lesson-5/visa/src/Pages/GayatriPage.jsx
+++ lesson-5/visa/src/Pages/GayatriPage.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import PhotoshopComponent from '../Components/Components-Button/PhotoshopComponent'
 
 const GayatriPage = props => {
     return (
         <>
             <div className="img1">
-
+                <PhotoshopComponent />
                 <img src="https://unsplash.it/50/50"></img>
                 <h1>Gayatri Tutorial</h1>
                 <img src="https://unsplash.it/50/50"></img>
