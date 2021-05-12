2e0d659acb9972c929a43e74e3778c6d2a8a1118
sonydaman <sonydaman@gmail.com>
2021-05-12 18:28:53 +0530

    input ky

---
+++ lesson-5/visa/src/components/Components-Button/InputButton.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const InputButton = props => {
+    return (
+        <div>
+            <button className="button">
+                input key
+            </button>
+        </div>
+    )
+}
+
+InputButton.propTypes = {
+
+}
+
+export default InputButton
