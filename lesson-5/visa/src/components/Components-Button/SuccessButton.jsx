43f3c072e38f828233c9946b22d55e20c3522567
sonydaman <sonydaman@gmail.com>
2021-05-12 18:24:10 +0530

    success

---
+++ lesson-5/visa/src/components/Components-Button/SuccessButton.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const SuccessButton = props => {
+    return (
+        <div>
+            <button className="button">
+                this is a key
+            </button>
+        </div>
+    )
+}
+
+SuccessButton.propTypes = {
+
+}
+
+export default SuccessButton
