26f31e3733ff86bb897694fc82593fb5ddb25593
sonydaman <sonydaman@gmail.com>
2021-05-12 17:52:44 +0530

    caps set

--- lesson-5/visa/src/Pages/Caps Page.jsx
+++ lesson-5/visa/src/Pages/Caps Page.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import propTypes from 'prop-types'
 import Achievement from '../Components/Achievement'
 import Achievements from '../Components/Achievements'
+import WarningButton from '../Components/Components-Button/WarningButton'
+
 export const CapsPage = props => {
     return (
         <div className="tab-page">
+            <WarningButton />
             <Achievements items={
                 [
                     { title: 'text1', img: 'https:unsplash.it/200/200', description: 'veritatis? Quaerat rem veniam nihil? Sequi commodi quibusdam iste voluptatibus omnis consequuntur, accusamus at voluptatum adipisci perferendis quod minima sunt, non velit nulla earum quis vel voluptates aut dignissimos alias dolorum? Nisi ut est iusto molestias inventore necessitatibus corrupti libero repellendus? Quisquam a assumenda officiis blanditiis accusantium nam, totam voluptas eius ad eveniet itaque ipsum vero aut rem dolore corrupti obcaecati! Aliquid, corporis. Corporis minus iure libero. Quod quisquam perferendis iste laudantium totam nisi rem hic ullam? Quos, provident nisi error, suscipit quae exercitationem perspiciatis nesciunt beatae modi enim, impedit molestiae! Quas provident, quae neque non sed aperiam accusantium illum labore recusandae consequatur explicabo laborum quo, doloribus sint perspiciatis, optio est magnam nobis! Obcaecati veniam asperiores molestias qui perferendis adipisci exercitationem. Atque deleniti necessitatibus debitis ullam sit nemo voluptatibus perferendis esse nam, dolor laborum. Tenetur molestias quia soluta laborum unde deserunt animi, rerum tempore ullam possimus, nulla voluptates omnis expedita alias. Aut, quasthis is a page this is a row this is column this is a queue this is a view' },
