ddf68c3d5053bf9e9b5ddb98b87484e012785624
sonydaman <sonydaman@gmail.com>
2021-05-12 15:53:10 +0530

    set default

--- lesson-5/visa/src/components/Components-Button/PrimaryButton.jsx
+++ lesson-5/visa/src/components/Components-Button/PrimaryButton.jsx
@@ -1,21 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import PrimaryButton from './PrimaryButton'
-import SecondryButton from './SecondryButton'
 
 const PrimaryButton = props => {
     return (
+
         <div>
-            <div className="test3">
-                <PrimaryButton />
-            </div>
-
-            <div className="test4">
-                <SecondryButton />
-            </div>
-
+            <button className="button">
+                hi this is a web button
+            </button>
         </div>
+
     )
+
 }
 
 PrimaryButton.propTypes = {
@@ -23,5 +19,3 @@ PrimaryButton.propTypes = {
 }
 
 export default PrimaryButton
-
-
