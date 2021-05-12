31f10b8ef899e44e58fe1489284f5fc25a06669e
sonydaman <sonydaman@gmail.com>
2021-05-12 17:53:32 +0530

    info set

---
+++ lesson-5/visa/src/components/Components-Button/InfoButton.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const InfoButton = props => {
+    return (
+        <div>
+            this is a info button
+        </div>
+    )
+}
+
+InfoButton.propTypes = {
+
+}
+
+export default InfoButton
+
