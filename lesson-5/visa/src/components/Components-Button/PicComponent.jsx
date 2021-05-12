1c1f7e33fa64a867faef9b6541808ff7099f0fdd
sonydaman <sonydaman@gmail.com>
2021-05-12 18:45:14 +0530

    pic set

---
+++ lesson-5/visa/src/components/Components-Button/PicComponent.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const PicComponent = props => {
+    return (
+        <div>
+            <div className="img">
+                <img src="https://unsplash.it/200/200"></img>
+            </div>
+        </div>
+    )
+}
+
+PicComponent.propTypes = {
+
+}
+
+export default PicComponent
