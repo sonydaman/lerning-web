64525f2b19988d8bf391a66f2a3ecb54bb95ac8d
sonydaman <sonydaman@gmail.com>
2021-05-12 17:52:17 +0530

    feature set

--- lesson-5/visa/src/Pages/FeaturePage.jsx
+++ lesson-5/visa/src/Pages/FeaturePage.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Achievement from '../Components/Achievement'
 import Achievements from '../Components/Achievements'
+import InfoButton from '../Components/Components-Button/InfoButton'
+
+
 
 const FeaturePage = props => {
     return (
         <div className="feature-page">
+            <InfoButton />
             <Achievements items={
                 [
                     { title: 'Test1', img: 'https://unsplash.it/200/200' },
