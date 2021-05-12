8102ac635430fcdc1440e551804acd1a5155cfea
sonydaman <sonydaman@gmail.com>
2021-05-12 18:05:00 +0530

    set default

---
+++ lesson-5/visa/src/components/Components-Button/DefaulltButton.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const DefaulltButton = props => {
+    return (
+        <div>
+            <buttton className="button">
+                hi this is a column
+            </buttton>
+        </div>
+    )
+}
+
+DefaulltButton.propTypes = {
+
+}
+
+export default DefaulltButton
