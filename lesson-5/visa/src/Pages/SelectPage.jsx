65667cef03e167145bdda660a95bcff8f61bbe1f
sonydaman <sonydaman@gmail.com>
2021-05-12 15:51:08 +0530

    select test

--- lesson-5/visa/src/Pages/SelectPage.jsx
+++ lesson-5/visa/src/Pages/SelectPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { BreadCrumb } from '../Components'
+import ImageComponent from '../Components/Components-Button/ImageComponent'
 // import { Link } from 'react-router-dom'
 
 export const SelectPage = () => {
     return (
 
         <div className="academic-page">
+            <ImageComponent />
             <BreadCrumb />
             <h2>Academic</h2>
             this is a test
