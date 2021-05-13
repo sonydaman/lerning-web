64843438f8164184dc2d1ad3be383e44c9bab4f4
MaddeySh <maddeysharma96@gmail.com>
2021-05-13 19:32:58 +0530

    Push For error

--- lesson-5/visa/src/Pages/WindowPage.jsx
+++ lesson-5/visa/src/Pages/WindowPage.jsx
@@ -6,6 +6,7 @@ export const WindowPage = () => {
 
         <div>
             {/* <Test name="windowxp" img="https:unsplash.it/200/200 id=4" desc="loadingSuscipit provident fugit alias quos aut impedit, eum doloremque porro repudiandae rem aliquid! Illo, dolor libero! Eum mollitia asperiores placeat, ullam iure impedit quibusdam assumenda earum at debitis iste fugit reprehenderit libero nisi dolores facilis corporis ipsam iusto magnam delectus unde quasi sapiente. Totam, inventore odit? Quia tenetur, mollitia ex deserunt porro amet delectus corrupti veritatis soluta minus, suscipit asperiores cupiditate explicabo sed maiores deleniti cum dolore nihil ut exercitationem! Maiores, eius minus! Vero, libero maiores? Voluptas provident ullam optio exercitationem! Provident nihil numquam iste tempora perferendis excepturi nesciunt, non veniam nobis nisi fugiat ex quos ducimus itaque ipsam deserunt impedit accusantium! Consequuntur. Et laboriosam dolore iusto, sequi" /> */}
+        Hy
         </div>
 
 
