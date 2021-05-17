eeddc4cc0f8207a3569834f247eaaca33fbfa496
parteek arora <parteekarora@gmail.comm>
2021-05-17 19:59:33 +0530

    change footer style

--- lesson-5/visa/src/components/Footer.jsx
+++ lesson-5/visa/src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Col, Row } from 'react-bootstrap'
+import {Button} from 'react-bootstrap'
 
 import "../style/footer.css"
 export const Footer = () => {
@@ -7,14 +8,18 @@ export const Footer = () => {
         <div className="footer">
                 <Row>
                     <Col lg={6} md={6} sm={6}>
-                        <div className="Copy-right-text">
-                            <p>Copyright By Learning Web</p>
+                        <div className="">
+                            <p>Join Funbook to learn, share knowledge, and build your career.
+
+</p>
                         </div>
                     </Col>
                     <Col lg={6} md={6} sm={6}>
-                        <div className="Policy">
-                            <p>Powered by Khushal</p>
-                        </div>
+                        
+                        <Button variant="light">Sign Up With Google</Button>
+                        <Button variant="light">Sign Up With Facebook</Button>
+                        <Button variant="dark">Sign Up With GitHub</Button>
+                        
                     </Col>
                 </Row>
         </div>
