a343e5fdf57e5e622437f7827c02ae76ba3b258a
parteek arora <parteekarora@gmail.comm>
2021-05-26 18:36:04 +0530

    ok

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -16,7 +16,7 @@ export const SideBar = () => {
                     <Link to="/new-products">New Products</Link>
                     </li>
                 </ul>
-        </nav>
+           </nav>
         </div>
     )
 }
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 
+
 export const Home = () => {
-    return (
+
+    return ( 
         <div>
-            Home
+            <section className="hero">
+                <div className="hero-body">
+                    <div className="container">
+                     <h1 className="title">
+                        Soaring to new heights                   
+                     </h1>
+                    </div>
+                    <div className="is-two-thirds column is paddingless">
+                       <h2 className="subtitle is-4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut autem, sapiente, quisquam dolorum ex aut dolores sequi officia porro quasi nesciunt quos rerum alias ipsam harum dolorem enim? Hic, ut.</h2>
+                    </div>
+                    <a className="button is-large is-primary" id="learn">
+                     Learn more
+                    </a>
+                </div>
+            </section>
         </div>
     )
 }
